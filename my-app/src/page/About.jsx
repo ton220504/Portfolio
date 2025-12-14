fe34e87990f8036451be50de580c9699a0d2b8c9
ton220504 <trantoan220504@gmail.com>
2025-12-14 22:15:13 +0700

    update continue day 14/12

--- my-app/src/page/About.jsx
+++ my-app/src/page/About.jsx
@@ -1,50 +1,51 @@
 import ScrollFloat from "../hook/ScrollFloat/ScrollFloat";
 import ScrollReveal from "../hook/ScrollReveal/ScrollReveal";
+import SplitText from "../hook/SplitText/SplitText";
 
 const About = () => {
+  const handleAnimationComplete = () => {
+    console.log('All letters have animated!');
+  };
   return (
-    <div className="min-h-[130vh] flex flex-col justify-center gap-3 text-center bg-gray-500 text-white ">
+    <div className="h-screen flex flex-col justify-center gap-3 text-center bg-gray-600 text-white ">
+      <div className="text-7xl font-bold">
+        <SplitText
+          text="Hello, I am a Software Developer passionate about technology,
+        interested in new technologies, and want to apply technology to daily life."
+          className="text-4xl font-semibold text-center"
+          delay={30}
+          duration={0.25}
+          ease="power3.out"
+          splitType="chars"
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: 1, y: 0 }}
+          threshold={0.1}
+          rootMargin="-100px"
+          textAlign="center"
+          onLetterAnimationComplete={handleAnimationComplete}
+        />
+      </div>
+      <div>
+        <SplitText
+          text="Xin chào, tôi là một lập trình viên đam mê công nghệ,
+        hứng thú với các công nghệ mới, mong muốn đem công nghệ vận dụng vào đời sống hàng ngày."
+          className="text-4xl font-semibold text-center"
+          delay={30}
+          duration={0.25}
+          ease="power3.out"
+          splitType="chars"
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: 1, y: 0 }}
+          threshold={0.1}
+          rootMargin="-100px"
+          textAlign="center"
+          onLetterAnimationComplete={handleAnimationComplete}
 
-      {/* <ScrollReveal
-        baseOpacity={0}
-        enableBlur={true}
-        baseRotation={5}
-        blurStrength={10}
-      >
-        Hello, I am a Software Developer passionate about technology,
-        interested in new technologies, and want to apply technology to daily life.
-      </ScrollReveal>
-
-      <ScrollReveal
-        baseOpacity={0}
-        enableBlur={true}
-        baseRotation={5}
-        blurStrength={10}
-      >
-        Xin chào, tôi là một lập trình viên đam mê công nghệ,
-        hứng thú với các công nghệ mới, mong muốn đem công nghệ vận dụng vào đời sống hàng ngày.
-      </ScrollReveal> */}
-      <ScrollFloat
-        animationDuration={1}
-        ease='back.inOut(2)'
-        scrollStart='center bottom+=50%'
-        scrollEnd='bottom bottom-=40%'
-        stagger={0.03}
-      >
-        Hello, I am a Software Developer passionate about technology,
-        interested in new technologies, and want to apply technology to daily life.
-      </ScrollFloat>
-      <ScrollFloat
-        animationDuration={1}
-        ease='back.inOut(2)'
-        scrollStart='center bottom+=50%'
-        scrollEnd='bottom bottom-=40%'
-        stagger={0.03}
-      >
-        Xin chào, tôi là một lập trình viên đam mê công nghệ,
-        hứng thú với các công nghệ mới, mong muốn đem công nghệ vận dụng vào đời sống hàng ngày.
-      </ScrollFloat>
+        />
+      </div>
     </div>
+
+
   )
 }
 
