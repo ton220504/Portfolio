191865ad84a2b6c407ed60a3e7deb9835b50af60
ton220504 <trantoan220504@gmail.com>
2025-12-16 00:40:13 +0700

    add Tilted Card day 16/12

--- my-app/src/page/About.jsx
+++ my-app/src/page/About.jsx
@@ -2,11 +2,9 @@ import GradientText from "../hook/GradientText/GradientText";
 import SplitText from "../hook/SplitText/SplitText";
 import { SiReact, SiNextdotjs, SiTypescript, SiTailwindcss, SiDocker, SiHtml5, SiCss3, SiJavascript, SiGit, SiGithub, SiMysql, SiNetapp, SiVercel } from 'react-icons/si';
 import LogoLoop from "../hook/LogoLoop/LogoLoop";
+import TiltedCard from "../hook/TiltedCard/TiltedCard";
 
 const About = () => {
-  const handleAnimationComplete = () => {
-    console.log('All letters have animated!');
-  };
 
   const techLogos = [
     { node: <SiReact />, title: "React", href: "https://react.dev" },
@@ -27,10 +25,12 @@ const About = () => {
 
   return (
     <div className="min-h-screen flex flex-col bg-black text-white overflow-hidden">
-      {/* ===== CONTENT TRÊN ===== */}
-      <div className="grid grid-cols-6 gap-4 px-6 pt-16">
-        {/* Title */}
-        <div className="col-start-2 col-span-4 text-center">
+
+      {/* ===== CONTENT ===== */}
+      <div className="grid grid-cols-6 gap-6 px-6 pt-16">
+
+        {/* ===== TITLE ===== */}
+        <div className="col-span-full md:col-start-2 md:col-span-4 text-center">
           <GradientText
             colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
             animationSpeed={3}
@@ -41,12 +41,12 @@ const About = () => {
           </GradientText>
         </div>
 
-        {/* Left text */}
-        <div className="col-span-6 md:col-start-1 md:col-end-4">
+        {/* ===== LEFT TEXT ===== */}
+        <div className="col-span-full md:col-span-3">
           <SplitText
             text="Hello, I am a Software Developer passionate about technology,
-          interested in new technologies, and want to apply technology to daily life."
-            className="text-lg md:text-2xl font-serif text-center"
+        interested in new technologies, and want to apply technology to daily life."
+            className="text-lg md:text-2xl font-serif text-center md:text-left"
             delay={30}
             duration={0.25}
             ease="power3.out"
@@ -57,8 +57,8 @@ const About = () => {
 
           <SplitText
             text="Xin chào, tôi là một lập trình viên đam mê công nghệ,
-          hứng thú với các công nghệ mới, mong muốn đem công nghệ vận dụng vào đời sống hàng ngày."
-            className="text-base md:text-xl text-center mt-4 font-serif"
+        hứng thú với các công nghệ mới, mong muốn đem công nghệ vận dụng vào đời sống hàng ngày."
+            className="text-base md:text-xl mt-4 font-serif text-center md:text-left"
             delay={30}
             duration={0.25}
             ease="power3.out"
@@ -68,13 +68,32 @@ const About = () => {
           />
         </div>
 
-        {/* Right placeholder */}
-        <div className=" md:flex col-span-2 items-center justify-center">
-          right
+        {/* ===== RIGHT CARD ===== */}
+        <div className="
+      col-span-full md:col-span-3
+      flex justify-center items-center
+      scale-75 sm:scale-90 md:scale-100
+      transition-transform
+    ">
+          <TiltedCard
+            imageSrc="/images/download.png"
+            altText="Trần Văn Toàn"
+            captionText="Trần Văn Toàn"
+            containerHeight="300px"
+            containerWidth="300px"
+            imageHeight="400px"
+            imageWidth="270px"
+            rotateAmplitude={12}
+            scaleOnHover={1.2}
+            showMobileWarning={false}
+            showTooltip={true}
+            displayOverlayContent={true}
+          />
         </div>
+
       </div>
 
-      {/* ===== ĐẨY LOGO XUỐNG ĐÁY ===== */}
+      {/* ===== PUSH LOGO TO BOTTOM ===== */}
       <div className="flex-1" />
 
       {/* ===== LOGO LOOP ===== */}
@@ -92,7 +111,9 @@ const About = () => {
           ariaLabel="Technology partners"
         />
       </div>
+
     </div>
+
   );
 }
 
