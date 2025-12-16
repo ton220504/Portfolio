0079e0041c634f66e579fd46bb7e4df7a109f21f
ton220504 <trantoan220504@gmail.com>
2025-12-17 00:29:21 +0700

    add Electron border day 17/12

--- my-app/src/page/Skill.jsx
+++ my-app/src/page/Skill.jsx
@@ -1,12 +1,60 @@
 import React from 'react'
+import ElectricBorder from '../hook/ElectricBorder/ElectricBorder'
 
 const Skill = () => {
-  return (
-    <div className='min-h-screen flex flex-col bg-black text-white'>
-        Skill
-        
-    </div>
-  )
+    return (
+        <div className='min-h-screen flex items-center justify-center bg-black text-white'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-6xl px-4'>
+                <div className='w-full md:w-[350px] mx-auto'>
+                    <ElectricBorder
+                        color="#7df9ff"
+                        speed={1}
+                        chaos={0.5}
+                        thickness={2}
+                        style={{ borderRadius: 16 }}
+                    >
+                        <div className="h-80 md:h-96 flex items-center justify-center">
+                            <p className="opacity-80 text-center">
+                                Front end
+                            </p>
+                        </div>
+                    </ElectricBorder>
+                </div>
+                <div className='w-full md:w-[350px] mx-auto'>
+                    <ElectricBorder
+                        color="#7df9ff"
+                        speed={1}
+                        chaos={0.5}
+                        thickness={2}
+                        style={{ borderRadius: 16 }}
+                    >
+                        <div className="h-80 md:h-96 flex items-center justify-center">
+                            <p className="opacity-80 text-center">
+                                Back end
+                            </p>
+                        </div>
+                    </ElectricBorder>
+                </div>
+                <div className='w-full md:w-[350px] mx-auto'>
+                    <ElectricBorder
+                        color="#7df9ff"
+                        speed={1}
+                        chaos={0.5}
+                        thickness={2}
+                        style={{ borderRadius: 16 }}
+                    >
+                        <div className="h-80 md:h-96 flex items-center justify-center">
+                            <p className="opacity-80 text-center">
+                                Other tools
+                            </p>
+                        </div>
+                    </ElectricBorder>
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Skill
