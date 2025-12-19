fac1a3a1e3cc347914aec67a653eeb3095586637
ton220504 <trantoan220504@gmail.com>
2025-12-19 21:34:06 +0700

    update Skill.jsx day 19/12

--- my-app/src/page/Skill.jsx
+++ my-app/src/page/Skill.jsx
@@ -1,5 +1,34 @@
 import React from 'react'
 import ElectricBorder from '../hook/ElectricBorder/ElectricBorder'
+import html from '../../public/images/html-5.png'
+import css from '../../public/images/css-3.png'
+import js from '../../public/images/js.png'
+import reactjs from '../../public/images/reactjs.png'
+import nextjs from '../../public/images/nextjs.png'
+import tailwind from '../../public/images/tailwind.png'
+import bootstrap from '../../public/images/bootstrap.png'
+import ts from '../../public/images/typescript.png'
+import nodejs from '../../public/images/nodejs.png'
+import express from '../../public/images/express.png'
+import mssql from '../../public/images/sql-server.png'
+import mysql from '../../public/images/mysql.png'
+import csharp from '../../public/images/c-sharp.png'
+import net from '../../public/images/dotNet-core.png'
+import fastify from '../../public/images/fastify.png'
+import oracle from '../../public/images/oracle.png'
+import git from '../../public/images/git.png'
+import github from '../../public/images/github.png'
+import gitlab from '../../public/images/gitlab.png'
+import masterialui from '../../public/images/material-UI.png'
+import vercel from '../../public/images/vercel.png'
+import stackoverflow from '../../public/images/stack-overflow.png'
+import npm from '../../public/images/npm.png'
+import nginx from '../../public/images/NGINX.png'
+import vscode from '../../public/images/vscode.png'
+import visualstudio from '../../public/images/visualstudio.png'
+import docker from '../../public/images/docker.png'
+import postman from '../../public/images/postman.png'
+
 
 const Skill = () => {
     return (
@@ -14,9 +43,16 @@ const Skill = () => {
                         style={{ borderRadius: 16 }}
                     >
                         <div className="h-80 md:h-96 flex items-center justify-center">
-                            <p className="opacity-80 text-center">
-                                Front end
-                            </p>
+                            <div className='grid grid-cols-3 gap-4'>
+                                <img src={html} alt="HTML5" className="w-15 h-15 mx-2" />
+                                <img src={css} alt="CSS3" className="w-15 h-15 mx-2" />
+                                <img src={js} alt="JavaScript" className="w-15 h-15 mx-2" />
+                                <img src={ts} alt="TypeScript" className="w-15 h-15 mx-2" />
+                                <img src={reactjs} alt="ReactJS" className="w-15 h-15 mx-2" />
+                                <img src={nextjs} alt="NextJS" className="w-15 h-15 mx-2" />
+                                <img src={tailwind} alt="TailwindCSS" className="w-15 h-15 mx-2" />
+                                <img src={bootstrap} alt="Bootstrap" className="w-15 h-15 mx-2" />
+                            </div>
                         </div>
                     </ElectricBorder>
                 </div>
@@ -29,9 +65,16 @@ const Skill = () => {
                         style={{ borderRadius: 16 }}
                     >
                         <div className="h-80 md:h-96 flex items-center justify-center">
-                            <p className="opacity-80 text-center">
-                                Back end
-                            </p>
+                            <div className='grid grid-cols-3 gap-4'>
+                                <img src={nodejs} alt="NodeJS" className="w-15 h-15 mx-2" />
+                                <img src={express} alt="ExpressJS" className="w-15 h-15 mx-2 bg-white" />
+                                <img src={csharp} alt="C#" className="w-15 h-15 mx-2" />
+                                <img src={net} alt=".NET Core" className="w-15 h-15 mx-2" />
+                                <img src={mssql} alt="MS SQL Server" className="w-15 h-15 mx-2 bg-white" />
+                                <img src={mysql} alt="MySQL" className="w-15 h-15 mx-2" />
+                                <img src={fastify} alt="Fastify" className="w-15 h-15 mx-2 bg-white" />
+                                <img src={oracle} alt="Oracle" className="w-15 h-15 mx-2 " />
+                            </div>
                         </div>
                     </ElectricBorder>
                 </div>
@@ -44,9 +87,21 @@ const Skill = () => {
                         style={{ borderRadius: 16 }}
                     >
                         <div className="h-80 md:h-96 flex items-center justify-center">
-                            <p className="opacity-80 text-center">
-                                Other tools
-                            </p>
+                            
+                            <div className='grid grid-cols-3 gap-4'>
+                                <img src={git} alt="Git" className="w-15 h-15 mx-2" />
+                                <img src={github} alt="GitHub" className="w-15 h-15 mx-2 bg-white" />
+                                <img src={gitlab} alt="GitLab" className="w-15 h-15 mx-2" />
+                                <img src={masterialui} alt="Material UI" className="w-15 h-15 mx-2" />
+                                <img src={vercel} alt="Vercel" className="w-15 h-15 mx-2 bg-white" />
+                                <img src={stackoverflow} alt="Stack Overflow" className="w-15 h-15 mx-2" />
+                                <img src={npm} alt="NPM" className="w-15 h-15 mx-2" />
+                                <img src={nginx} alt="NGINX" className="w-15 h-15 mx-2" />
+                                <img src={vscode} alt="VS Code" className="w-15 h-15 mx-2" />
+                                <img src={visualstudio} alt="Visual Studio" className="w-15 h-15 mx-2" />
+                                <img src={docker} alt="Docker" className="w-15 h-15 mx-2" />
+                                <img src={postman} alt="Postman" className="w-15 h-15 mx-2" />
+                            </div>
                         </div>
                     </ElectricBorder>
                 </div>
